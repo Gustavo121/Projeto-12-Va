64d1949a21f7978584302a78b0f4f854a06de10f
Gustavo <gustgouvea@hotmail.com>
2021-11-25 11:26:48 -0300

    subindo altereções projeto12

--- setup/app.js
+++ setup/app.js
@@ -22,3 +22,39 @@ const weekdays = [
   "Saturday",
 ];
 
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2021, 11, 25, 10, 41, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent =  `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60min
+  // 1d = 24hrs
+
+  // values in ms
+  const oneDay = 24*60*60*1000;
+  console.log(oneDay);
+}
+
+getRemainingTime();
